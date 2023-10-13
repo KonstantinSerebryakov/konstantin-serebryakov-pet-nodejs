67fe75f891d17cc211bb7d287750a93342e6e4af
KonstantinSerebryakov <KonstantinSerebryakov2000@gmail.com>
2023-10-13 18:08:43 +0300

    deploy initial

--- apps/accounts/src/main.ts
+++ apps/accounts/src/main.ts
@@ -8,6 +8,23 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   console.log(process.env.AMQP_EXCHANGE);
   console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  console.log(process.env.AMQP_EXCHANGE);
+  Logger.log(process.env.AMQP_EXCHANGE);
+  Logger.log(process.env.AMQP_EXCHANGE);
+  Logger.log(process.env.AMQP_EXCHANGE);
+  Logger.log(process.env.AMQP_EXCHANGE);
+  Logger.log(process.env.AMQP_EXCHANGE);
 	const app = await NestFactory.create(AppModule);
 	await app.init();
 	Logger.log(
